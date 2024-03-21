29fc679a0f9d8054790b7b1f12d76c26dcd03e65
kawagoxtosin <163124116+kawagoxtosin@users.noreply.github.com>
2024-03-21 12:06:44 -0300

    Add files via upload

---
+++ number.js
@@ -0,0 +1,20 @@
+// tipo Number
+
+const meunumero = 3;
+
+const primeironumero = 1;
+const segundonumero = 2;
+const terceironumero = 2.1;
+
+let soma = primeironumero + segundonumero;
+let subtracao = primeironumero - segundonumero;
+let multiplicacao = primeironumero * segundonumero;
+let divisao = primeironumero / segundonumero;
+
+console.log("A soma é",soma)
+console.log("A subtracao é",subtracao)
+console.log("A multiplicacao é",multiplicacao)
+console.log("a divisao é",divisao,"\n")
+
+
+//-------------------
---
+++ string.js
@@ -0,0 +1,19 @@
+//tipo String
+
+const texto1 = "Olá, mundo!";
+const texto2 = "Olá, mundo!";
+const senha =  "senha@superSegura456!";
+const StringdeNumeros = "34567";
+
+const citacao = "Meu nome é ";
+const meuNome = "Kawa";
+
+// concatenação (+)
+
+console.log(texto1)
+console.log(texto2)
+console.log(senha)
+console.log(StringdeNumeros)
+console.log(citacao + meuNome)
+
+//template string OU template Literal
