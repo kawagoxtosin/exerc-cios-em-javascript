2b89748ce3741eed19ac15c616a840b44b7bf879
kawagoxtosin <163124116+kawagoxtosin@users.noreply.github.com>
2024-03-22 08:13:34 -0300

    Add files via upload

---
+++ booleans.js
@@ -0,0 +1,19 @@
+// boolean
+// true -> verdadeiro
+// true -> falso
+const primeironumero = 5;
+const segundonumero = 5;
+let cadastroAtivado = true;
+
+//comparação das variaveis com operadores
+
+console.log(primeironumero === segundonumero);//igual
+console.log(primeironumero != segundonumero);//diferente
+console.log(primeironumero < segundonumero);//menor
+console.log(primeironumero > segundonumero);//maior
+console.log(primeironumero <= segundonumero);//menor ou igual
+console.log(primeironumero >= segundonumero);//maior ou igual
+
+const texto1 = "altura";
+const texto2 ="altura";
+console.log(texto1 === texto2)
