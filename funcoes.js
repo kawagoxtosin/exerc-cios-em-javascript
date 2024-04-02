cbf03ded63e9b655be80dd0a7802410d77ff0369
kawagoxtosin <163124116+kawagoxtosin@users.noreply.github.com>
2024-04-02 11:15:01 -0300

    Add files via upload

---
+++ funcoes.js
@@ -0,0 +1,28 @@
+function imprimetexto(texto) {
+ console.log(texto);
+}
+
+imprimetexto("oi, amigos") 
+
+function texto(text){
+    return text;
+}
+console.log(texto("parametro"))
+
+function soma(num1,num2){
+    return num1+num2;
+}
+function subtracao(num1,num2){
+    return num1-num2;
+}
+function divisao(num1,num2){
+    return num1/num2; 
+}
+function multiplicacao(num1,num2){
+    return num1*num2; 
+}
+console.log("A soma dos números é", soma(3,2))
+console.log("A divisão dos números é",divisao(3,2))
+console.log("A subtração dos números é",subtracao(3,2))
+console.log("A multiplicação dos números é", multiplicacao(3,2))
+
